c5e915fa07cab9de220542ca3f8083593899d8c7
Jed Llenado <jedlovescpe2@gmail.com>
2018-12-10 00:15:54 +0300

    Completed desktop and header styles.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,27 +2,35 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     browserSync = require('browser-sync').create();
 
-gulp.task('watch',function(){
+gulp.task('watch',function() {
     browserSync.init({
         notify: false,
-        server:{
+        server: {
             baseDir: "app"
         },
         open: false
     });
 
-    watch('./app/index.html', function(){
+    watch('./app/index.html', function () {
         browserSync.reload();
     });
 
-    watch('./app/assets/styles/**/*.css',function(){
+    watch('./app/assets/styles/**/*.css', function () {
         gulp.start('cssInject')
         console.log('Styles are successfully generated.')
 
+    });
+
+    watch('./app/assets/scripts/**/*.js', function () {
+        gulp.start('scriptsRefresh');
     });
 });
 
 gulp.task('cssInject',['styles'],function(){
     return gulp.src('./app/temp/styles/styles.css')
         .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh',['scripts'], function(){
+    browserSync.reload();
 });
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: "./app/assets/scripts/App.js",
+    output: {
+        path: __dirname + "/app/temp/scripts",
+        filename: "App.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
