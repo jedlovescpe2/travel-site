59abc0f3bda66cb0dd753560803e785451545743
Jed Llenado <jedlovescpe2@gmail.com>
2018-12-12 14:19:45 +0300

    Completed sticky header functionality.

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,41 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll{
+    constructor(els,offset){
+
+        this.itemsToReveal = els;
+        this.offsetPercentage = offset;
+
+        this.hideInitially();
+        this.createWayPoints();
+
+
+    }
+
+    hideInitially(){
+        this.itemsToReveal.addClass('reveal-item');
+    }
+
+    createWayPoints(){
+
+        var self = this;
+        this.itemsToReveal.each(function(){
+
+            var currentItem = this;
+            //console.log(currentItem[0].attr('class'));
+
+            new Waypoint({
+               element: currentItem,
+               handler: function(){
+                   $(currentItem).addClass('reveal-item--is-visible');
+               },
+               offset: self.offsetPercentage
+           });
+
+
+        });
+    }
+}
+
+export default RevealOnScroll;
